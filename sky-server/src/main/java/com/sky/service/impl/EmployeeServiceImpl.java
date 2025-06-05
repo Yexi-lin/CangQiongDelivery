1cedbaf75da135960986544adf783966ddbdb7bb
yexi_lin <yexi_lin@qq.com>
2025-06-05 11:29:53 +0800

    新增员工分页查询接口 完善注释 http消息日期格式转换

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +23,7 @@ import org.springframework.util.DigestUtils;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -80,6 +85,18 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.addEmployee(employee);
+    }
+
+    @Override
+    public PageResult getEmpPage(EmployeePageQueryDTO dto) {
+        //1.启用分页插件
+        PageHelper.startPage(dto.getPage(), dto.getPageSize());
+        //2.查询sql获取emp集合 并封装为Page
+        Page<Employee> pageList = employeeMapper.getEmpPage(dto);
+        //获取分页的数据总数和列表
+        long total = pageList.getTotal();
+        List<Employee> result = pageList.getResult();
+        return new PageResult(total, result);
     }
 
 }
